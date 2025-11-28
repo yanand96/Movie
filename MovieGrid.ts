65de25f51169c1f07f7b1d15a34625b3de2ebc55
ANAND KUMAR YADAV <yanand647@gmail.com>
2025-11-28 15:50:42 +0530

    Add files via upload

---
+++ MovieGrid.ts
@@ -0,0 +1,70 @@
+﻿
+namespace MovieTutorial.Default {
+    
+    @Serenity.Decorators.registerClass()
+    export class MovieGrid extends Serenity.EntityGrid<MovieRow, any> {
+        protected getColumnsKey() { return 'Default.Movie'; }
+        protected getDialogType() { return MovieDialog; }
+        protected getIdProperty() { return MovieRow.idProperty; }
+        protected getLocalTextPrefix() { return MovieRow.localTextPrefix; }
+        protected getService() { return MovieService.baseUrl; }
+
+        constructor(container: JQuery) {
+            super(container);
+        }
+
+        protected getQuickSearchFields(): Serenity.QuickSearchField[] {
+            let fld = MovieRow.Fields;
+            let txt = (s) => Q.text("Db." +
+                MovieRow.localTextPrefix + "." + s).toLowerCase();
+            return [
+                { name: "", title: "all" },
+                { name: fld.Description, title: txt(fld.Description) },
+                { name: fld.Storyline, title: txt(fld.Storyline) },
+                { name: fld.Year, title: txt(fld.Year) }
+            ];
+        }
+        protected getButtons() {
+            var buttons = super.getButtons();
+
+            buttons.push(Common.ExcelExportHelper.createToolButton({
+                grid: this,
+                service: MovieService.baseUrl + '\ListExcel',
+                onViewSubmit: () => this.onViewSubmit(),
+                separator: true
+            }));
+
+            buttons.push(Common.PdfExportHelper.createToolButton({
+                grid: this,
+                onViewSubmit: () => this.onViewSubmit(),
+                reportTitle: 'Movie List',
+                columnTitles: {
+                    'Discontinued': 'Dis.',
+                },
+                tableOptions: {
+                    columnStyles: {
+                        ProductID: {
+                            columnWidth: 25,
+                            halign: 'right'
+                        },
+                        Discountinued: {
+                            columnWidth: 25
+                        }
+                    }
+                }
+            }));
+
+           /* buttons.push({
+                title: 'Save Changes',
+                cssClass: 'apply-changes-button disabled',
+                onClick: e => this.saveClick(),
+                separator: true
+            });*/
+
+            return buttons;
+        }
+        saveClick() {
+            throw new Error("Method not implemented.");
+        }
+    }
+}
